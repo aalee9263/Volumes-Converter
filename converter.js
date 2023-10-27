59cddf5df25e60f99492828a8c39d447ca456b44
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-10-27 22:56:03 +0500

    JavaScript Added

--- converter.js
+++ converter.js
@@ -1,22 +1,21 @@
-function convertUnits(event) {
-    event.preventDefault(); // Prevent the form from submitting
-
-    const liter = parseFloat(document.getElementById("liter").value);
-    const milliliter = parseFloat(document.getElementById("milliliter").value);
-    // Add more unit variables here
-
-    const conversionFactors = {
-        liter: 1,
-        milliliter: 1000,
-        // Add more conversion factors here
+function convertVolume(unit) {
+    let units = {
+        liters: 1,
+        milliliters: 1000,
+        cubicmeters: 0.001,
+        cubiccentimeters: 1000,
+        fluidounces: 33.814,
+        pints: 2.113,
+        quarts: 1.057,
+        gallons: 0.264
     };
 
-    // Perform conversions for each unit
-    for (const unit in conversionFactors) {
-        const result = (liter * conversionFactors[unit]) / conversionFactors['liter'];
-        document.getElementById(unit).value = result.toFixed(2);
-    }
+    let value = parseFloat(document.getElementById(unit).value);
 
-    // Update the result element
-    document.getElementById("result").textContent = "Conversion completed!";
+    for (let otherUnit in units) {
+        if (otherUnit !== unit) {
+            let convertedValue = value * units[unit] / units[otherUnit];
+            document.getElementById(otherUnit).value = convertedValue;
+        }
+    }
 }
